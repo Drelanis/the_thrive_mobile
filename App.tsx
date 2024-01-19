cf5daf600916d407d1c2efc614fcf23dce0f605a
Denys <denisbadakaty51@gmail.com>
2024-01-19 11:53:31 +0200

    add app providers

--- App.tsx
+++ App.tsx
@@ -1,17 +1,3 @@
-import { config } from '@gluestack-ui/config';
-import { GluestackUIProvider } from '@gluestack-ui/themed';
-import { NavigationContainer } from '@react-navigation/native';
-
-import { MyDrawer } from '$app';
-
-const App = () => {
-  return (
-    <GluestackUIProvider config={config}>
-      <NavigationContainer>
-        <MyDrawer />
-      </NavigationContainer>
-    </GluestackUIProvider>
-  );
-};
+import { App } from '$app';
 
 export default App;
--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -1,29 +1,13 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Text, View } from 'react-native';
 
-const Drawer = createDrawerNavigator();
+import { AppProviders } from '../providers';
 
-const Feed = () => {
+export const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed Screen</Text>
-    </View>
-  );
-};
-
-const Article = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Article Screen</Text>
-    </View>
-  );
-};
-
-export const MyDrawer = () => {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Feed" component={Feed} />
-      <Drawer.Screen name="Article" component={Article} />
-    </Drawer.Navigator>
+    <AppProviders>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Article Screen</Text>
+      </View>
+    </AppProviders>
   );
 };
