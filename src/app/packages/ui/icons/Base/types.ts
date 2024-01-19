003c5857e1baf6a491867c433adba8d402d950f2
Denys <denisbadakaty51@gmail.com>
2024-01-19 13:52:57 +0200

    add icons and icon factory

---
+++ src/app/packages/ui/icons/Base/types.ts
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+type Props = {
+  color?: string;
+  size?: number;
+};
+
+export type BaseIcon = FC<Props>;
---
+++ src/app/packages/ui/icons/EyeOn/index.tsx
@@ -0,0 +1,5 @@
+import { EyeIcon } from 'lucide-react-native';
+
+import { IconFactory } from '../Base';
+
+export const EyeOnIcon = IconFactory(EyeIcon);
