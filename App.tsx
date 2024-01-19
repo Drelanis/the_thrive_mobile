ecae3213893ab1b465835b120f4a3f685dee3fee
Denys <denisbadakaty51@gmail.com>
2024-01-19 11:43:20 +0200

    add aliases

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { config } from '@gluestack-ui/config';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { MyDrawer } from './src/app';
+import { MyDrawer } from '$app';
 
 const App = () => {
   return (
