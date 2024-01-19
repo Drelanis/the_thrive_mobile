7316a3ec81e1653f64545592a7896dbc1809ce9f
Denys <denisbadakaty51@gmail.com>
2024-01-19 16:10:24 +0200

    add input and iput password

--- src/app/modules/Auth/screens/SignIn/components/SignInForm/index.tsx
+++ src/app/modules/Auth/screens/SignIn/components/SignInForm/index.tsx
@@ -1,35 +1,13 @@
-import { Input, InputField, InputSlot, VStack } from '@gluestack-ui/themed';
-import { useState } from 'react';
+import { VStack } from '@gluestack-ui/themed';
 import { StyleSheet } from 'react-native';
 
-import { EyeOffIcon, EyeOnIcon } from '$ui';
+import { Input, InputPassword } from '$ui';
 
 export const SignInForm = () => {
-  const [isVisible, setVisible] = useState(false);
-
-  const showPassword = () => setVisible(true);
-
-  const hidePassword = () => setVisible(false);
-
   return (
     <VStack style={styles.container}>
-      <Input variant="underlined" size="md">
-        <InputField
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Enter email"
-        />
-      </Input>
-      <Input variant="underlined" size="md">
-        <InputField
-          type={isVisible ? 'text' : 'password'}
-          placeholder="Enter password"
-        />
-        <InputSlot onPress={isVisible ? hidePassword : showPassword}>
-          {isVisible && <EyeOnIcon size={20} color="black" />}
-          {!isVisible && <EyeOffIcon size={20} color="black" />}
-        </InputSlot>
-      </Input>
+      <Input placeholder="Enter your email" />
+      <InputPassword />
     </VStack>
   );
 };
