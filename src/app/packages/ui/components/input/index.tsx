0fd41baa13df1ee70f062156e05112f78942b4fc
Denys <denisbadakaty51@gmail.com>
2024-01-20 16:03:51 +0200

    update input

--- src/app/packages/ui/components/input/index.tsx
+++ src/app/packages/ui/components/input/index.tsx
@@ -1,20 +1,40 @@
-import { Input as GluestackInput, InputField } from '@gluestack-ui/themed';
+import {
+  Input as GluestackInput,
+  InputField,
+  Text,
+  VStack,
+} from '@gluestack-ui/themed';
+import { Control, FieldValues, Path, useController } from 'react-hook-form';
 
-type Props = {
+type Props<Type extends FieldValues> = {
+  control: Control<Type>;
+  name: Path<Type>;
   placeholder?: string;
+  title?: string;
 };
 
-export const Input = (props: Props) => {
-  const { placeholder } = props;
+export const Input = <Type extends FieldValues>(props: Props<Type>) => {
+  const { placeholder, name, control, title } = props;
+
+  const {
+    field: { value, onChange },
+    fieldState: { error },
+  } = useController({ name, control });
 
   return (
-    <GluestackInput variant="underlined" size="md">
-      <InputField
-        autoCorrect={false}
-        autoCapitalize="none"
-        placeholder={placeholder}
-      />
-    </GluestackInput>
+    <VStack>
+      {title && <Text>{title}</Text>}
+      <GluestackInput isInvalid={Boolean(error)} variant="underlined" size="md">
+        <InputField
+          value={value}
+          onChangeText={onChange}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder={placeholder}
+        />
+      </GluestackInput>
+      {error && <Text>{error.message}</Text>}
+    </VStack>
   );
 };
 
