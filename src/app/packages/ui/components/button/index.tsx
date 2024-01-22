9f81ac8c9a4528cc4533ae7d2e1fec00cf4bafd1
Denys <denisbadakaty51@gmail.com>
2024-01-22 17:26:20 +0200

    add configs

--- src/app/packages/ui/components/button/index.tsx
+++ src/app/packages/ui/components/button/index.tsx
@@ -1,16 +1,21 @@
 import { Button as GluestackButton, ButtonText } from '@gluestack-ui/themed';
 import { PropsWithChildren } from 'react';
-import { StyleSheet } from 'react-native';
+import { PressableProps, StyleSheet } from 'react-native';
 
 type Props = {
   isDisabled?: boolean;
-} & PropsWithChildren;
+} & PropsWithChildren &
+  PressableProps;
 
 export const Button = (props: Props) => {
-  const { children, isDisabled = false } = props;
+  const { children, isDisabled = false, ...restProps } = props;
 
   return (
-    <GluestackButton isDisabled={isDisabled} style={styles.button}>
+    <GluestackButton
+      isDisabled={isDisabled}
+      style={styles.button}
+      {...restProps}
+    >
       <ButtonText>{children}</ButtonText>
     </GluestackButton>
   );
---
+++ src/configs/types.ts
@@ -0,0 +1,9 @@
+export type CompanyType = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  currency_id: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
