6693313397a669677e5dba7547ad2d01b8929954
Denys <denisbadakaty51@gmail.com>
2024-01-23 13:28:03 +0200

    add drawer navigation

--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -1,27 +1,10 @@
-import { createStackNavigator } from '@react-navigation/stack';
-
+import { StackNavigation } from './navigation';
 import { AppProviders } from './providers';
-
-import { SignIn } from '$modules/Auth';
-import { Home } from '$modules/Home';
-
-const Stack = createStackNavigator();
 
 export const App = () => {
   return (
     <AppProviders>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="SignIn"
-          component={SignIn}
-        />
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Home"
-          component={Home}
-        />
-      </Stack.Navigator>
+      <StackNavigation />
     </AppProviders>
   );
 };
---
+++ src/app/layout/navigation/drawer/components/LogoutItem.tsx
@@ -0,0 +1,42 @@
+import { View } from '@gluestack-ui/themed';
+import {
+  DrawerContentComponentProps,
+  DrawerItem,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native';
+
+import { Screens, ScreenStackType } from '$layout/types';
+
+export const LogoutItem = (props: DrawerContentComponentProps) => {
+  const { navigate } = useNavigation<StackNavigationProp<ScreenStackType>>();
+
+  const handleLogout = () => {
+    navigate(Screens.SIGN_IN);
+  };
+
+  return (
+    <View style={styles.container} {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        style={styles.drawerItem}
+        label="Logout"
+        onPress={handleLogout}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 8,
+    marginTop: 64,
+    marginBottom: 32,
+  },
+  drawerItem: {
+    marginTop: 'auto',
+  },
+});
