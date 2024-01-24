c9cb134d052c32cc620444297c69d01eba8ac332
Denys <denisbadakaty51@gmail.com>
2024-01-24 17:57:54 +0200

    minor fix

--- src/app/modules/Ideas/index.tsx
+++ src/app/modules/Ideas/index.tsx
@@ -1,28 +1,11 @@
-import { Center } from '@gluestack-ui/themed';
-import { useForm } from 'react-hook-form';
+import { Center, Text } from '@gluestack-ui/themed';
 import { StyleSheet } from 'react-native';
 
-import { Select } from '$ui';
-
 export const Ideas = () => {
-  const { control } = useForm({
-    values: {
-      select: [''],
-    },
-  });
-
-  const a = () => console.log('lox');
-
   return (
     <Center style={styles.container}>
-      {/* <Text>Ideas Screen</Text>
-      <Text>Still in progress</Text> */}
-      <Select
-        name="select"
-        control={control}
-        label="Simple label"
-        onPress={a}
-      />
+      <Text>Ideas Screen</Text>
+      <Text>Still in progress</Text>
     </Center>
   );
 };
