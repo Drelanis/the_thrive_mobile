c92b03a353d056b3965491a1cbdbb6a1eb244799
Denys <denisbadakaty51@gmail.com>
2024-01-24 13:29:22 +0200

    add FullScreenModal

--- src/app/layout/providers/index.tsx
+++ src/app/layout/providers/index.tsx
@@ -2,13 +2,16 @@ import { config } from '@gluestack-ui/config';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { NavigationContainer } from '@react-navigation/native';
 import { PropsWithChildren } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export const AppProviders = (props: PropsWithChildren) => {
   const { children } = props;
 
   return (
-    <GluestackUIProvider config={config}>
-      <NavigationContainer>{children}</NavigationContainer>
-    </GluestackUIProvider>
+    <SafeAreaProvider>
+      <GluestackUIProvider config={config}>
+        <NavigationContainer>{children}</NavigationContainer>
+      </GluestackUIProvider>
+    </SafeAreaProvider>
   );
 };
