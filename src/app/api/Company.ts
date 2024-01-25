9f73db2e2ca87e4b3115491584cc5775c160db9e
Denys <denisbadakaty51@gmail.com>
2024-01-25 19:33:56 +0200

    update company api

--- src/app/api/Company.ts
+++ src/app/api/Company.ts
@@ -17,6 +17,11 @@ class Company extends FactoryApi {
     const currentCompany = data.find((company) => company.email === email);
     return currentCompany;
   }
+
+  public async create(companyDto: CompanyType): Promise<CompanyType> {
+    const data = await this.post('/companies', companyDto);
+    return data;
+  }
 }
 
 export const companyApi = new Company();
