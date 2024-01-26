7bc8e9b7b6352673fab55db835d63d049f08a4e3
Denys <denisbadakaty51@gmail.com>
2024-01-26 13:05:53 +0200

    update config add types

--- src/configs/types.ts
+++ src/configs/types.ts
@@ -1,14 +1,23 @@
+export type DirectionType = {
+  direction: string;
+  description: string;
+};
+
+export type LocationType = {
+  country: string;
+  city: string;
+  street: string;
+  buildingNumber: string;
+};
+
 export type CompanyType = {
   id: string;
   name: string;
   email: string;
   password: string;
+  directions: (string | undefined)[];
+  location: LocationType;
   currency_id: string;
   createdAt: Date;
   updatedAt: Date;
-};
-
-export type DirectionType = {
-  direction: string;
-  description: string;
 };
