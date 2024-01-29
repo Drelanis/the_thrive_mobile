8ed7bb37cbe201ef0163a8c2b4bc8fe86e37aab5
Denys <denisbadakaty51@gmail.com>
2024-01-29 11:56:33 +0200

    minor fix

--- src/app/modules/Auth/screens/SignUp/index.tsx
+++ src/app/modules/Auth/screens/SignUp/index.tsx
@@ -7,7 +7,14 @@ import { useLogic } from './useLogic';
 
 import { ScreenBackground } from '$app/packages/common';
 import { signUpStore } from '$app/stores';
-import { ArrowLeft, Button, Header, Input, InputType } from '$ui';
+import {
+  ArrowLeft,
+  Button,
+  Header,
+  Input,
+  InputType,
+  KeyboardAvoidingView,
+} from '$ui';
 
 export const SignUp = () => {
   const styles = useStyles();
@@ -18,66 +25,68 @@ export const SignUp = () => {
     useLogic();
 
   return (
-    <ScreenBackground>
-      <Header
-        goBack={goBack}
-        goBackIcon={ArrowLeft}
-        title="COMPANY REGISTRATION"
-      />
-      <ScrollView style={styles.container}>
-        <VStack style={styles.form}>
-          <Input
-            type={InputType.TEXT}
-            placeholder="Enter your company name"
-            control={control}
-            name="name"
-          />
-          <Input
-            type={InputType.TEXT}
-            placeholder="Enter your company email"
-            control={control}
-            name="email"
-          />
-          <Input
-            type={InputType.PASSWORD}
-            placeholder="Enter your password"
-            control={control}
-            name="password"
-          />
-          <Input
-            type={InputType.PASSWORD}
-            placeholder="Repeat your password"
-            control={control}
-            name="repeatPassword"
-          />
-          <SelectDirections
-            initialState={signUpStore.signUp.directions}
-            getValues={getValues}
-            resetField={resetField}
-            name="directions"
-            control={control}
-          />
-          <AddressForm
-            initialState={signUpStore.signUp.location}
-            getValues={getValues}
-            resetField={resetField}
-            control={control}
-            name="location"
-            setValue={setValue}
-          />
-          <Input
-            type={InputType.TEXT}
-            placeholder="Enter your number of employees"
-            control={control}
-            name="numberOfEmployees"
-            mask="9999"
-          />
-          <Button isDisabled={!isValid} onPress={onSubmit}>
-            REGISTRATE
-          </Button>
-        </VStack>
-      </ScrollView>
-    </ScreenBackground>
+    <KeyboardAvoidingView>
+      <ScreenBackground>
+        <Header
+          goBack={goBack}
+          goBackIcon={ArrowLeft}
+          title="COMPANY REGISTRATION"
+        />
+        <ScrollView style={styles.container}>
+          <VStack style={styles.form}>
+            <Input
+              type={InputType.TEXT}
+              placeholder="Enter your company name"
+              control={control}
+              name="name"
+            />
+            <Input
+              type={InputType.TEXT}
+              placeholder="Enter your company email"
+              control={control}
+              name="email"
+            />
+            <Input
+              type={InputType.PASSWORD}
+              placeholder="Enter your password"
+              control={control}
+              name="password"
+            />
+            <Input
+              type={InputType.PASSWORD}
+              placeholder="Repeat your password"
+              control={control}
+              name="repeatPassword"
+            />
+            <SelectDirections
+              initialState={signUpStore.signUp.directions}
+              getValues={getValues}
+              resetField={resetField}
+              name="directions"
+              control={control}
+            />
+            <AddressForm
+              initialState={signUpStore.signUp.location}
+              getValues={getValues}
+              resetField={resetField}
+              control={control}
+              name="location"
+              setValue={setValue}
+            />
+            <Input
+              type={InputType.TEXT}
+              placeholder="Enter your number of employees"
+              control={control}
+              name="numberOfEmployees"
+              mask="9999"
+            />
+            <Button isDisabled={!isValid} onPress={onSubmit}>
+              REGISTRATE
+            </Button>
+          </VStack>
+        </ScrollView>
+      </ScreenBackground>
+    </KeyboardAvoidingView>
   );
 };
 
