ea1abe8d7f3a6df1c9415b0b7774ada5a3f510ac
Denys <denisbadakaty51@gmail.com>
2024-02-23 15:33:28 +0200

    add draft components for sign up company

---
+++ src/app/packages/ui/draft/companySignUp/validation/index.ts
@@ -0,0 +1,28 @@
+import * as yup from 'yup';
+
+import { ValidationHints } from '$packages/configs';
+
+export const signUpValidationSchema = yup.object({
+  name: yup.string().required(ValidationHints.REQUIRED),
+  email: yup
+    .string()
+    .required(ValidationHints.REQUIRED)
+    .email(ValidationHints.INVALID_EMAIL),
+  password: yup.string().required(ValidationHints.REQUIRED),
+  repeatPassword: yup
+    .string()
+    .required(ValidationHints.REQUIRED)
+    .test(
+      'passwords-match',
+      ValidationHints.PASSWORDS_MISMATCH,
+      function (value) {
+        return this.parent.password === value;
+      },
+    ),
+  directions: yup
+    .array(yup.string())
+    .required()
+    .min(1, ValidationHints.REQUIRED),
+  location: yup.string().required(ValidationHints.REQUIRED),
+  numberOfEmployees: yup.string().required(ValidationHints.REQUIRED),
+});
--- src/app/stores/signUp.ts
+++ src/app/stores/signUp.ts
@@ -1,41 +1,15 @@
-import { createMultiScreenStore } from './store';
-
 export type SignUpStoreType = {
-  signUp: {
-    name: string;
-    email: string;
-    password: string;
-    repeatPassword: string;
-    directions: (string | undefined)[];
-    location: string;
-    numberOfEmployees: string;
-  };
-  address: {
-    country: string;
-    remote: boolean;
-    city?: string;
-    street?: string;
-    buildingNumber?: string;
-  };
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
 };
 
 export const signUpStore: SignUpStoreType = {
-  signUp: {
-    name: '',
-    email: '',
-    password: '',
-    repeatPassword: '',
-    directions: [],
-    location: '',
-    numberOfEmployees: '',
-  },
-  address: {
-    country: '',
-    city: '',
-    street: '',
-    buildingNumber: '',
-    remote: false,
-  },
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  repeatPassword: '',
 };
-
-export const useSignUpCreationStore = createMultiScreenStore(signUpStore);
