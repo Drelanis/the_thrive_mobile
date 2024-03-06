818c792fd9c84e9331a873edac758d1680eef9d7
Denys <denisbadakaty51@gmail.com>
2024-03-06 12:59:20 +0200

    minor fix

--- src/configs/types.ts
+++ src/configs/types.ts
@@ -1,30 +1,5 @@
 import { UserRoles } from './constants';
 
-export type DirectionType = {
-  direction: string;
-  description: string;
-};
-
-export type LocationType = {
-  country: string;
-  remote: boolean;
-  city?: string;
-  street?: string;
-  buildingNumber?: string;
-};
-
-export type CompanyType = {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-  directions: (string | undefined)[];
-  location: LocationType;
-  currency_id: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
-
 export type ScreenStackType = {
   SignIn: undefined;
   SignUp: undefined;
